2b835836b70e87eee0dd7111eb804dcc51893f5d
kadir4172 <kcimenci@hotmail.com>
2016-07-29 16:17:54 +0300

    [update] compile script is updated

--- proj.linux/main.cpp
+++ proj.linux/main.cpp
@@ -12,6 +12,8 @@ int main(int argc, char **argv)
 {
     // create the application instance
     AppDelegate app;
+    std::cout << "appdelegate adresi:" << &app << std::endl;
+    std::cout << "application adresi:" << Application::getInstance() << std::endl;
     std::cout << "Oyun baslatiliyor" << std::endl;
     return Application::getInstance()->run();
 }
